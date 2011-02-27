6ca13eca1ecb68c3c1d873633c7fc96621ae18fd
Dominik Sacha <dominiksacha@Dominik-Sachas-MacBook-Pro.local>
2011-02-27 19:49:17 +0100

    databasehandler fertig

--- src/prototype/DaD_test2/Classes/DataHandler.m
+++ src/prototype/DaD_test2/Classes/DataHandler.m
@@ -20,31 +20,66 @@
 
 -(NSString*) getWordFromFile {
 	
+	//Pfade setzen
 	NSArray *p = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES); 
 	NSString *filePath = [NSString stringWithFormat:@"%@/%@",[p objectAtIndex:0],@"Words.txt"];
 	NSString *srcPath = [[NSBundle mainBundle] pathForResource:wordsFilePath ofType:@"txt"];
 	
+	//Wenn keine Wortdatei im appducumentsordner angelegt wurde: anlegen!
 	if (![[NSFileManager defaultManager] fileExistsAtPath:filePath]) {
 		
 		[[NSFileManager defaultManager] copyItemAtPath: srcPath toPath:filePath error:NULL];
 	}
-	//
 	
+	//Wortdatei auslesen
 	NSData *fileData = [NSData dataWithContentsOfFile:filePath];
 	NSString* words = [[NSString alloc] initWithData:fileData encoding:NSASCIIStringEncoding];
 	
+	//Woerter in aarz speichern
 	NSMutableArray *lines = [words componentsSeparatedByString:@"\n"]; // each line, adjust character for line endings
+	
+	//nur unbenutzte Woerter benutzen
+	NSMutableArray *unusedWords = [[NSMutableArray alloc]init];
+	for (NSString *word in lines) {
+		if (![word hasPrefix:@"#"]) {
+			[unusedWords addObject:word];
+		}
+	}
+	
+	//wenn keine woerter mehr uebrug sind: neue liste
+	if ([unusedWords count] == 0) {
+		int c = 0;
+		NSMutableArray *temp = [[NSMutableArray alloc]init];
+		for (NSString *word in lines) {	
+			[temp insertObject:[word substringFromIndex:1] atIndex:c];
+			c++;
+		}
+		lines = temp;
+		unusedWords = temp;
+	}
 	
 	//Get a Random word:
 	int randPos = 0;
-	randPos = (rand()%([lines count]-1));
-	NSString *newWord = [[NSString alloc] initWithString:[lines objectAtIndex:randPos]];
+	randPos = (rand()%([unusedWords count]));
+	
+	//kennzeichnen des benutzten wortes
+	NSString *newWord = [[NSString alloc] initWithString:[unusedWords objectAtIndex:randPos]];
 	NSMutableString *hashed = [[NSMutableString alloc] initWithString: @"#"];
 	[hashed appendString:newWord];
-	[lines replaceObjectAtIndex:randPos withObject:hashed];
-	NSLog([lines objectAtIndex:randPos]);
+	
+	int count = 0;
+	int found = 0;
+	for (NSString* word in lines) {
+		if (word == newWord) {
+			found = count;
+		}
+		count = count+1;
+	}
+	
+	[lines replaceObjectAtIndex:found withObject:hashed];
 	[hashed release];
 	
+	//rausschreiben der neuen Wortliste
 	NSMutableString *newStr = [[NSMutableString alloc] init];
 	for (NSString *s in lines) {
 		[newStr appendString:s];
@@ -52,8 +87,6 @@
 		if(![s isEqual:[lines objectAtIndex:(i-1)]])
 			[newStr appendString:@"\n"];
 	}
-
-	
 	BOOL statu = [newStr writeToFile:filePath atomically:YES];
 	
 	return newWord;
