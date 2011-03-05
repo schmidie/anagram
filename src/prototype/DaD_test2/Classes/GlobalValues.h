88eaf9b7736823abc3ac430ca6e611e221b6e711
Schmidie <toschmid@fh-konstanz.de>
2011-03-05 14:12:02 +0100

    global values

---
+++ src/prototype/DaD_test2/Classes/GlobalValues.h
@@ -0,0 +1,11 @@
+//
+//  GlobalValues.h
+//  DaD_test2
+//
+//  Created by tobi on 3/1/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#define modusTraining @"Training"
+
+#define modusProfi @"Profi"
---
+++ src/prototype/DaD_test2/Classes/GlobalValues.m
@@ -0,0 +1,9 @@
+//
+//  GlobalValues.m
+//  DaD_test2
+//
+//  Created by tobi on 3/1/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "GlobalValues.h"
